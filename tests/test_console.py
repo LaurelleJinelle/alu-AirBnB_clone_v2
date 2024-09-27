437d90d7f88f5e9ced10f0f31a49f4c0796e8361
irielle12 <i.irakoze@alustudent.com>
2024-09-27 10:48:36 -0700

    .

--- tests/test_console.py
+++ tests/test_console.py
@@ -28,14 +28,14 @@ class TestConsoleDocs(unittest.TestCase):
 
     def test_console_module_docstring(self):
         """Test for the console.py module docstring"""
-        self.assertIsNot(console.__doc__, None,
+        self.assertIsNot(console._doc_, None,
                          "console.py needs a docstring")
-        self.assertTrue(len(console.__doc__) >= 1,
+        self.assertTrue(len(console._doc_) >= 1,
                         "console.py needs a docstring")
 
     def test_HBNBCommand_class_docstring(self):
         """Test for the HBNBCommand class docstring"""
-        self.assertIsNot(HBNBCommand.__doc__, None,
+        self.assertIsNot(HBNBCommand._doc_, None,
                          "HBNBCommand class needs a docstring")
-        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
+        self.assertTrue(len(HBNBCommand._doc_) >= 1,
                         "HBNBCommand class needs a docstring")
